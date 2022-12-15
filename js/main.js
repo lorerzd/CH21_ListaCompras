9dc2bb5134c4bb53a62cb045fe6161e328eec4e2
lorena <lore.rdz2802@gmail.com>
2022-12-15 13:42:04 -0600

    Se agrego la tabla con los productos y cantidades

--- js/main.js
+++ js/main.js
@@ -14,6 +14,14 @@ let tabla = document.getElementById("tablaListaCompras");
 let cuerpoTabla = tabla.getElementsByTagName("tbody");
 let alertValidaciones = document.getElementById("alertValidaciones");
 let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+let contadorProductos = document.getElementById("contadorProductos");
+let productosTotal = document.getElementById("productosTotal");
+let precioTotal = document.getElementById("precioTotal");
+let contador=0;
+let totalEnProductos=0;
+let costoTotal=0;
+let precio=0;
+let cantidad=0;
 let idTimeout;
 let btnAgregar = document.getElementById("btnAgregar");
 
@@ -56,13 +64,39 @@ btnAgregar.addEventListener ("click", function(event){
     txtNumber.style.border = "red thin solid";
     lista += "<li>Se debe escribir una cantidad válida</li>"
    }
-   
+   lista += "</ul>";
    alertValidacionesTexto.insertAdjacentHTML("beforeend", lista);
    alertValidaciones.style.display = "block";
    idTimeout = setTimeout (function (){
     alertValidaciones.style.display = "none";
    }, 5000)
+   return false;
 
-   }//if validaciones
+ }//if validaciones
+
+ txtNombre.style.border ="";
+ txtNumber.style.border ="";
+ alertValidaciones.style.display = "none";
+ contador++;
+ contadorProductos.innerHTML = contador;
+  cantidad = parseFloat(txtNumber.value);
+  totalEnProductos += cantidad;
+  productosTotal.innerHTML= totalEnProductos;
+  precio = getPrecio();
+  costoTotal += precio * cantidad;
+  precioTotal.innerHTML = "$" + costoTotal.toFixed(2);
+  
+  let row = `<tr> 
+        <td>${contador} </td> 
+        <td>${txtNombre.value} </td> 
+        <td>${txtNumber.value} </td>
+        <td>${precio} </td> 
+  </tr>`;
+
+   cuerpoTabla[0].insertAdjacentHTML("beforeend", row);
+   txtNombre.value="";
+   txtNumber.value="";
+   txtNombre.focus();
+
 
 });
